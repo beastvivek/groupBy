d487725bb4b1a5371a6dbcac1720e592b8818898
Vivek Bisht <99522211+beastvivek@users.noreply.github.com>
2022-04-06 17:27:37 +0530

    Implemented without recursion

--- areEqual.js
+++ areEqual.js
@@ -1,13 +1,27 @@
-const areEqual = function (lhs, rhs) {
-  if (!Array.isArray(lhs) || !Array.isArray(rhs)) {
-    return lhs === rhs;
+const isArray = Array.isArray;
+
+const areEqual = function (LHS, RHS) {
+  const isOneOfThemNotArray = !(isArray(LHS) && isArray(RHS));
+  if (isOneOfThemNotArray) {
+    return LHS === RHS;
   }
-  if (lhs.length !== rhs.length) {
-    return false;
-  }
-  for (let index = 0; index < lhs.length; index++) {
-    if (!areEqual(lhs[index], rhs[index])) {
+  const stack = [[LHS, RHS]];
+
+  while (stack.length !== 0) {
+    const bothArrays = stack.pop();
+    const lhsArray = bothArrays[0];
+    const rhsArray = bothArrays[1];
+
+    if (lhsArray.length !== rhsArray.length) {
       return false;
+    }
+
+    for (let index = 0; index < lhsArray.length; index++) {
+      if (isArray(lhsArray[index]) && isArray(rhsArray[index])) {
+        stack.push([lhsArray[index], rhsArray[index]]);
+      } else if (lhsArray[index] !== rhsArray[index]) {
+        return false;
+      }
     }
   }
   return true;
